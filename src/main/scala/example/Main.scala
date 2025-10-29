3bd82db247116877007342c2d6375ac122b5f4e2
David Flores <2879168-devfloresitver@users.noreply.gitlab.com>
2025-10-28 23:54:15 -0600

    commented main for now

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -3,38 +3,37 @@ package example
 import coloring.RGBColor
 import com.sksamuel.scrimage.ImmutableImage
 import com.sksamuel.scrimage.webp.WebpWriter
-import web.Utilsold
 
 import java.io.{File, IOException}
 
 object ImageUtils extends  App{
-
-  private val ic =   Utilsold("input", "output")
-  ic.build() match {
-    case Right(ready) =>
-      ready.convertToWebp()
-      ready.createThumbnail("thumb_", "mobile")
-
-    case Left(error) =>
-      println(s"Error: $error")
-  }
-
-  val base = RGBColor(100, 50, 200)
-  val bright = base.increaseAll(30, 30, 30)
-  println(bright) // RGBColor(130,80,230)
-  val other = RGBColor(255, 0, 0)
-  val mixed = base.mixWith(other, 0.5)
-  println(mixed) // RGBColor(177,25,100)
-  println(mixed.toHex) // #b11964
-  ic.generatePlaceholders(2, 100,100, Some(new RGBColor(20,20,20)))
-  val input = ImmutableImage.loader().fromFile(new File("input.jpg"))
-  val processed = ic.OCR.prepareOCR(
-    image = input,
-    tilt = -1,
-    contrastFactor = 1.5,
-    threshold = 140
-  )
-  ic.OCR.saveAsJpeg(processed, "output_ocr_ready.jpg")
-  println("OCR-ready")
+//
+//  private val ic =   Utilsold("input", "output")
+//  ic.build() match {
+//    case Right(ready) =>
+//      ready.convertToWebp()
+//      ready.createThumbnail("thumb_", "mobile")
+//
+//    case Left(error) =>
+//      println(s"Error: $error")
+//  }
+//
+//  val base = RGBColor(100, 50, 200)
+//  val bright = base.increaseAll(30, 30, 30)
+//  println(bright) // RGBColor(130,80,230)
+//  val other = RGBColor(255, 0, 0)
+//  val mixed = base.mixWith(other, 0.5)
+//  println(mixed) // RGBColor(177,25,100)
+//  println(mixed.toHex) // #b11964
+//  ic.generatePlaceholders(2, 100,100, Some(new RGBColor(20,20,20)))
+//  val input = ImmutableImage.loader().fromFile(new File("input.jpg"))
+//  val processed = ic.OCR.prepareOCR(
+//    image = input,
+//    tilt = -1,
+//    contrastFactor = 1.5,
+//    threshold = 140
+//  )
+//  ic.OCR.saveAsJpeg(processed, "output_ocr_ready.jpg")
+//  println("OCR-ready")
 
 }
